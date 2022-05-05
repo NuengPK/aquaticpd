0df072f7a62776bfe47e86e997eefae40937d287
Nueng Pasit <onetwotee1@gmail.com>
2022-05-05 15:36:32 +0700

    Navigation on Header Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { headerComponent } from './header/header.component';
 import { AquaticFoodListComponent } from './aquatic-foods/aquatic-food-list/aquatic-food-list.component';
-import { AquaticDistributionListComponent } from './Distribution-List/aquatic-distribution-list/aquatic-distribution-list.component';
+import { AquaticDistributionListComponent } from './Distribution-List/aquatic-distribution-list.component';
 import { AquaticDistributionEditComponent } from './Distribution-List/aquatic-distribution-edit/aquatic-distribution-edit.component';
-import { AquaticFoodsComponent } from './aquatic-foods/aquatic-foods/aquatic-foods.component';
 import { AquaticFoodItemComponent } from './aquatic-foods/aquatic-food-item/aquatic-food-item.component';
 import { AquaticFoodDetailComponent } from './aquatic-foods/aquatic-food-detail/aquatic-food-detail.component';
+import { AquaticFoodsComponent } from './aquatic-foods/aquatic-foods.component';
 
 @NgModule({
   declarations: [
