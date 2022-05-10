fb7865adb331602a34a06f464875cb5fc189ffad
Nueng Pasit <onetwotee1@gmail.com>
2022-05-10 14:01:51 +0700

    Big Edit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { headerComponent } from './header/header.component';
 import { AquaticFoodListComponent } from './aquatic-foods/aquatic-food-list/aquatic-food-list.component';
-import { AquaticDistributionListComponent } from './Distribution-List/aquatic-distribution-list.component';
-import { AquaticDistributionEditComponent } from './Distribution-List/aquatic-distribution-edit/aquatic-distribution-edit.component';
 import { AquaticFoodItemComponent } from './aquatic-foods/aquatic-food-item/aquatic-food-item.component';
 import { AquaticFoodDetailComponent } from './aquatic-foods/aquatic-food-detail/aquatic-food-detail.component';
 import { AquaticFoodsComponent } from './aquatic-foods/aquatic-foods.component';
+import { DistributionListComponent } from './distribution-list/distribution-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     headerComponent,
     AquaticFoodListComponent,
-    AquaticDistributionListComponent,
-    AquaticDistributionEditComponent,
     AquaticFoodsComponent,
     AquaticFoodItemComponent,
-    AquaticFoodDetailComponent
+    AquaticFoodDetailComponent,
+    DistributionListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/distribution-list/distribution-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-distribution-list',
+  templateUrl: './distribution-list.component.html',
+  styleUrls: ['./distribution-list.component.css']
+})
+export class DistributionListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
