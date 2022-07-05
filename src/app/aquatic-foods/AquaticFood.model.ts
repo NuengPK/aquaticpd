d908975eed9fd11643a066545bb39d0be5526554
Nueng <onetwotee1@gmail.com>
2022-07-05 10:43:03 +0700

    qty & onHand

--- src/app/aquatic-foods/AquaticFood.model.ts
+++ src/app/aquatic-foods/AquaticFood.model.ts
@@ -2,10 +2,14 @@ export class AquaticFood {
   public name: string;
   public description: string;
   public imagePath: string;
+  public quantity:number;
+  public onHand:number;
 
-  constructor(name: string, desc: string, imagePath: string){
+  constructor(name: string, desc: string, imagePath: string, qty:number, onHand:number){
     this.name = name;
     this.description = desc;
     this.imagePath = imagePath;
+    this.quantity = qty;
+    this.onHand = onHand;
 }
 }
--- src/app/shared/aquatic-food.service.ts
+++ src/app/shared/aquatic-food.service.ts
@@ -1,8 +1,10 @@
+import { AquaticFood } from "../aquatic-foods/AquaticFood.model"
+
 export class AquaticFoodService{
 
-  private aquaticFoods = [
-    {name: "ปลาอินทรี", description: "กินที อร่อยทั้งเรือน", imagePath: "http://4.bp.blogspot.com/-oBRtL82nIJc/UhxGa2sB1LI/AAAAAAAAAF0/k1CEbItGXEU/s1600/%E0%B8%9B%E0%B8%A5%E0%B8%B2%E0%B8%AD%E0%B8%B4%E0%B8%99%E0%B8%97%E0%B8%A3%E0%B8%B5%E0%B8%A2%E0%B9%8C+11.jpg"},
-    {name: "หมึก", description: "กินที อร่อยทั้งเรือน", imagePath: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Arrow_squid.jpg/300px-Arrow_squid.jpg"}
+  private aquaticFoods:AquaticFood[] = [
+    new AquaticFood("ปลาอินทรี", "กินที อร่อยทั้งเรือน", "http://4.bp.blogspot.com/-oBRtL82nIJc/UhxGa2sB1LI/AAAAAAAAAF0/k1CEbItGXEU/s1600/%E0%B8%9B%E0%B8%A5%E0%B8%B2%E0%B8%AD%E0%B8%B4%E0%B8%99%E0%B8%97%E0%B8%A3%E0%B8%B5%E0%B8%A2%E0%B9%8C+11.jpg",70,0),
+    new AquaticFood("หมึก", "กินที อร่อยทั้งเรือน", "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Arrow_squid.jpg/300px-Arrow_squid.jpg",100,0)
   ]
   getAquaticFoods(){
     return this.aquaticFoods
