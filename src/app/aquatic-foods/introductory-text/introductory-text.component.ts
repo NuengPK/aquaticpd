77e3ab4239dce08168ca99441694ddfef4b5f7af
nueng <onetwotee1@gmail.com>
2022-08-26 22:03:49 +0700

    generate introductory-text+'กรุณาเลือกรายการสินค้า' And edit font/css

---
+++ src/app/aquatic-foods/introductory-text/introductory-text.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-introductory-text',
+  templateUrl: './introductory-text.component.html',
+  styleUrls: ['./introductory-text.component.css']
+})
+export class IntroductoryTextComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
